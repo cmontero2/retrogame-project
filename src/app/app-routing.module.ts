824bf28ff7c3a72d4b00a541b427221721dab5ad
flucero <ferlucero89@gmail.com>
2021-01-16 20:45:42 +0100

    ficheros faltantes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SeccionesComponent } from './foro/secciones/secciones.component';
+import { EntradasComponent } from './foro/entradas/entradas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'foro/secciones', component: SeccionesComponent },
+  { path: 'foro/entradas', component: EntradasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SeccionesComponent } from './foro/secciones/secciones.component';
+import { EntradasComponent } from './foro/entradas/entradas.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { CategoriasService } from './categorias/categorias.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
+    SeccionesComponent,
+    EntradasComponent,
+    CategoriasComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CategoriasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

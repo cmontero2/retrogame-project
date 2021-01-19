624a8c4b3f77d07e90e82aa78d6ce7d011125a3a
cmontero2 <cmontero@iesfuentesanluis.org>
2021-01-19 12:54:10 +0100

    footer-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { SeccionesComponent } from './foro/secciones/secciones.component';
 import { EntradasComponent } from './foro/entradas/entradas.component';
 import { CategoriasComponent } from './categorias/categorias.component';
 import { CategoriasService } from './categorias/categorias.service';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './home/footer/footer.component';
 import { NoticiasComponent } from './noticias/noticias.component';
 import { NoticiasService } from "./noticias/noticias.service";
 

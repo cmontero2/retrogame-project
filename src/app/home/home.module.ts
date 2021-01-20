976b5d29d653f307fae9f2a25fd5bbbc85df4f66
Cristina Montero <cmontero@iesfuentesanluis.org>
2021-01-20 14:59:44 +0100

    botones hover-active y modulo login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeContainerComponent } from './home-container/home-container.component';
 import { ScrollBarComponent } from './scroll-bar/scroll-bar.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [HomeContainerComponent, ScrollBarComponent],
+  declarations: [HomeContainerComponent, ScrollBarComponent, LoginComponent],
   imports: [
     CommonModule
   ],

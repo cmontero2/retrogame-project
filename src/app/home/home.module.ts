f3ecb470fad4eb5e72181771a46d319cd0cb225b
Cristina Montero <cmontero@iesfuentesanluis.org>
2021-01-20 13:30:40 +0100

    intento scrollbar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeContainerComponent } from './home-container/home-container.component';
+import { ScrollBarComponent } from './scroll-bar/scroll-bar.component';
 
 
 @NgModule({
-  declarations: [HomeContainerComponent],
+  declarations: [HomeContainerComponent, ScrollBarComponent],
   imports: [
     CommonModule
   ],

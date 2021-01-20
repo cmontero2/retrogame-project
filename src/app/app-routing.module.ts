fedefbde3e96b47a6e976e4ade2f64d9a8ecb563
Cristina Montero <cmontero@iesfuentesanluis.org>
2021-01-20 21:39:03 +0100

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeContainerComponent } from './home/home-container/home-container.component';
 import { SeccionesComponent } from './foro/secciones/secciones.component';
 import { EntradasComponent } from './foro/entradas/entradas.component';
+import { LoginComponent } from './home/login/login.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeContainerComponent},
   { path: 'foro/secciones', component: SeccionesComponent },
-  { path: 'foro/entradas', component: EntradasComponent }
+  { path: 'foro/entradas', component: EntradasComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({

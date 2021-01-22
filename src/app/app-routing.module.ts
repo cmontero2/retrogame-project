391bcb3e36eaa17ae60dfd3b3d6304aaa2751936
Cristina Montero <cmontero@iesfuentesanluis.org>
2021-01-22 14:32:10 +0100

    registro y routing de juegos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SeccionesComponent } from './foro/secciones/secciones.component';
 import { EntradasComponent } from './foro/entradas/entradas.component';
 import { LoginComponent } from './home/login/login.component';
 import { CategoriaDetalleComponent } from './juegos/categoria-detalle/categoria-detalle.component';
+import { RegistroUserComponent } from './home/registroUser/registro-user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'foro/secciones', component: SeccionesComponent },
   { path: 'foro/entradas', component: EntradasComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'juegos', component: CategoriaDetalleComponent }
+  { path: 'juegos', component: CategoriaDetalleComponent },
+  { path: 'registroUser', component: RegistroUserComponent }
 ];
 
 @NgModule({

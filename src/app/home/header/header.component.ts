c833e077c4dc547b698a0ce3f6df48a730f9a5ed
flucero <ferlucero89@gmail.com>
2021-01-27 20:58:02 +0100

    Añadidas depentencias de ngModal (hacer npm install para que baje las librerias al node-modules) + modal en login

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,29 +1,36 @@
 import { Component } from '@angular/core';
 import { AuthGuardService } from '../../services/auth-guard.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
-  })
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
 export class HeaderComponent {
   login: boolean = false;
 
-  constructor(private auth: AuthGuardService){
+  constructor(private auth: AuthGuardService, private modalService: NgbModal) {
     this.auth.isLoggedIn()
-    .subscribe(
-      data => {
-        this.login = data;
-        console.log('login',this.login);
-        
-      },
-      error => {
-        console.log(error);
-      });
+      .subscribe(
+        data => {
+          this.login = data;
+          console.log('login', this.login);
+
+        },
+        error => {
+          console.log(error);
+        });
 
   }
 
-  logout(){
+  doLogin() {
+    console.log("entrando");
+    const modalRef = this.modalService.open(LoginComponent);
+  }
+
+  logout() {
     this.auth.logout();
   }
 }
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthGuardService } from '../../services/auth-guard.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,19 @@ import { AuthGuardService } from '../../services/auth-guard.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  
+  private userName?: String;
+  private userPassword?: String;
 
-  constructor(private auth:AuthGuardService) { }
+  constructor(private auth:AuthGuardService, public activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
+   
   }
 
   login(){
     this.auth.login();
+    this.activeModal.close();
   }
 
   logout(){

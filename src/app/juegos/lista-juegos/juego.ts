e39ca4d3af03907189cd87b044a509f8fce97f0d
DavideZenobi <davidezenobigarcia@gmail.com>
2021-01-27 10:23:01 +0100

    cambios metodos getJuegos

--- src/app/juegos/lista-juegos/juego.ts
+++ src/app/juegos/lista-juegos/juego.ts
@@ -1,13 +1,18 @@
 export class Juego {
     id?: number;
-    categoria?: string;
+    categoriasId?: string;
+    categoriaNombre?: string[];
     nombre?: string;
     imagen?: string;
     descripcion?: string;
 
-    constructor(id: number, categoria: string, nombre: string, imagen: string, descripcion: string) {
+    constructor(id: number, categoriasId: string, categoriaNombre: string[], nombre: string, imagen: string, descripcion: string) {
         this.id = id;
-        this.categoria = categoria;
+        for(let i = 0; i < categoriaNombre.length; i++) {
+            this.categoriasId = categoriasId[i];
+            this.categoriaNombre?.push(categoriaNombre[i]);
+        }
+        this.categoriaNombre = categoriaNombre;
         this.nombre = nombre;
         this.imagen = imagen;
         this.descripcion = descripcion;
--- src/app/juegos/lista-juegos/lista-juegos.component.ts
+++ src/app/juegos/lista-juegos/lista-juegos.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Juego } from './juego';
 import { ListaJuegosService } from './lista-juegos.service';
 import { ActivatedRoute } from '@angular/router';
-import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-lista-juegos',
@@ -13,11 +12,14 @@ export class ListaJuegosComponent implements OnInit {
 
   juegos?: Juego[];
   categoria?: string;
-  constructor(private listaJuegosService: ListaJuegosService, private route: ActivatedRoute) { }
+  constructor(
+    private listaJuegosService: ListaJuegosService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(parameters => {
-      this.juegos = this.listaJuegosService.getJuegosCategoria(parameters.id);
+      this.juegos = this.listaJuegosService.getJuegosPorCategoriaId(parameters.id);
     })
 
   }

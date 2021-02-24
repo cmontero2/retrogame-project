11f039a7c2cf92fa84764f0c90bd986c28ebfa4a
cmontero2 <cmontero@iesfuentesanluis.org>
2021-02-24 12:39:10 +0100

    por fin se acabo

--- src/app/services/app.constants.ts
+++ src/app/services/app.constants.ts
@@ -1,3 +1,4 @@
 export const URL_API = {
-    SERVER_URL_API: "http://localhost/retrogame-projectAPI/web/"
+    //SERVER_URL_API: "http://localhost/retrogame-projectAPI/web/"
+    SERVER_URL_API: "http://alum1.iesfsl.org:8081/"
 }
